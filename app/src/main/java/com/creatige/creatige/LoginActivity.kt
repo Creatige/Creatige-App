e15b8dee090b6b9e2a83b53c047ce68181bb69ee
Eli Mojica <85600606+Elmojica@users.noreply.github.com>
2022-11-15 22:30:27 -0500

    parse has been implemented with back4app

--- app/src/main/java/com/creatige/creatige/LoginActivity.kt
+++ app/src/main/java/com/creatige/creatige/LoginActivity.kt
@@ -2,10 +2,14 @@ package com.creatige.creatige
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
     }
 }
