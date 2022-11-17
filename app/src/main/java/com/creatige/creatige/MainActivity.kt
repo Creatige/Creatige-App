ca30a56fd21f587d007061bd0ccee32ab6607553
pedropachecog <pmpg@hotmail.com>
2022-11-17 18:31:49 -0500

    Added Feed and Profile fragments, and navigation now works

--- app/src/main/java/com/creatige/creatige/MainActivity.kt
+++ app/src/main/java/com/creatige/creatige/MainActivity.kt
@@ -2,12 +2,62 @@ package com.creatige.creatige
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.creatige.creatige.fragments.CreateTextFragment
+import com.creatige.creatige.fragments.FeedFragment
+import com.creatige.creatige.fragments.ProfileFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-}
 
-//test
+
+        val fragmentManager: FragmentManager = supportFragmentManager
+
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).setOnItemSelectedListener {
+
+                item ->
+
+            var fragmentToShow : Fragment? = null
+
+            when(item.itemId){
+
+                R.id.action_feed -> {
+
+                    fragmentToShow = FeedFragment()
+
+//                    Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
+                }
+
+                R.id.action_create -> {
+                    fragmentToShow = CreateTextFragment()
+                }
+
+                R.id.action_profile -> {
+                    fragmentToShow = ProfileFragment()
+
+//                    Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            if (fragmentToShow!=null){
+
+//                Toast.makeText(this, "Fragment is not null", Toast.LENGTH_SHORT).show()
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragmentToShow).commit()
+            }
+
+            // returns true to say that we have handled this user interaction
+            true
+        }
+
+
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).selectedItemId = R.id
+            .action_create
+
+
+    }
+
+}
