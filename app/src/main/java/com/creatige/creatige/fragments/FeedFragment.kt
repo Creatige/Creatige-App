d883f34d832208d93fd6d0b4a97a15d2658e87d6
stevenkyritsis <16929888+stevenkyritsis@users.noreply.github.com>
2022-11-23 23:34:14 -0500

    Update FeedFragment.kt

--- app/src/main/java/com/creatige/creatige/fragments/FeedFragment.kt
+++ app/src/main/java/com/creatige/creatige/fragments/FeedFragment.kt
@@ -84,6 +84,35 @@ open class FeedFragment : Fragment() {
 
     }
 
+    open fun searchDB(searchItem : String){
+        //specify which class to query
+        val query: ParseQuery<posts> = ParseQuery.getQuery(posts::class.java)
+        query.include(posts.KEY_PROMPT)
+
+
+
+
+        query.findInBackground(object : FindCallback<posts> {
+            override fun done(posts: MutableList<posts>?, e: ParseException?){
+                if(e != null){
+                    Log.e(TAG, "Error fetching posts")
+                } else {
+                    if (posts != null){
+                        for(post in posts){
+                            Log.i(TAG, "Post:" + post.getPrompt()+ ", username: "+ post.getUser()?.username)
+                        }
+                        allPosts.clear()
+                        allPosts.addAll(posts)
+                        adapter.notifyDataSetChanged()
+                        //TODO: Implement the logic to set the swipecontainer to stop spinning around like its really silly for spinning around really
+                        //swipeContainer.setRefreshing(false)
+                    }
+                }
+
+            }
+        })
+    }
+
     companion object{
         const val TAG = "FeedFragment"
     }
