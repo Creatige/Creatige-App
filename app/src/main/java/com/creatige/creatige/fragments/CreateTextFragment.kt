e2d856c6aa464792d09beff17d8cfc914a3fe630
Bruno Mota <bruno.ed.mota@gmail.com>
2022-11-27 18:34:28 -0500

    Added xml for advanced options

--- app/src/main/java/com/creatige/creatige/fragments/CreateTextFragment.kt
+++ app/src/main/java/com/creatige/creatige/fragments/CreateTextFragment.kt
@@ -8,9 +8,12 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.codepath.asynchttpclient.AsyncHttpClient
@@ -33,6 +36,8 @@ class CreateTextFragment : Fragment() {
     var params = RequestParams()
     lateinit var parseFile: ParseFile
     var post= posts()
+    var expanded = false
+    lateinit var spinner: Spinner
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -93,8 +98,24 @@ class CreateTextFragment : Fragment() {
         // Remove this and set to the actual image we get back from the API
         ivGenerated = view.findViewById<ImageView>(R.id.ivGenerated)
         btnGenerate = view.findViewById<Button>(R.id.btnGenerate)
-
-
+        spinner = view.findViewById(R.id.spinner)
+        view.findViewById<Button>(R.id.options_expand).setOnClickListener {
+            if (expanded){
+                view.findViewById<LinearLayout>(R.id.adv_list).visibility = View.GONE
+                expanded = false
+            }else{
+                view.findViewById<LinearLayout>(R.id.adv_list).visibility = View.VISIBLE
+                expanded = true
+            }
+        }
+        ArrayAdapter.createFromResource(
+            view.context,
+            R.array.sampler,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter= adapter
+        }
         btnGenerate.setOnClickListener{
             val prompt = view.findViewById<EditText>(R.id.etPrompt).text.toString()
             val user = ParseUser.getCurrentUser()
