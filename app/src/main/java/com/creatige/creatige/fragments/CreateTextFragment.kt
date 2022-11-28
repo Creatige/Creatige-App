651ce4feb3cf255f9b4af7d7150703cafe31e09d
Eli Mojica <85600606+Elmojica@users.noreply.github.com>
2022-11-28 06:35:59 -0500

    reorganizing create

--- app/src/main/java/com/creatige/creatige/fragments/CreateTextFragment.kt
+++ app/src/main/java/com/creatige/creatige/fragments/CreateTextFragment.kt
@@ -34,18 +34,104 @@ class CreateTextFragment : Fragment() {
     lateinit var ivGenerated: ImageView
     lateinit var btnGenerate: Button
     lateinit var etPrompt: EditText
+    lateinit var parseFile: ParseFile
+    lateinit var spinner: Spinner
+
     val JSON = "application/json; charset=utf-8".toMediaTypeOrNull()
     var requestHeaders = RequestHeaders()
     var params = RequestParams()
-    lateinit var parseFile: ParseFile
     var post = posts()
     var expanded = false
-    lateinit var spinner: Spinner
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
+    
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_text, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        etPrompt = view.findViewById<EditText>(R.id.etPrompt)
+        ivGenerated = view.findViewById<ImageView>(R.id.ivGenerated)
+        btnGenerate = view.findViewById<Button>(R.id.btnGenerate)
+        etPrompt = view.findViewById<EditText>(R.id.etPrompt)
+        spinner = view.findViewById(R.id.spinner)
+        view.findViewById<Button>(R.id.options_expand).setOnClickListener {
+            if (expanded) {
+                view.findViewById<LinearLayout>(R.id.adv_list).visibility = View.GONE
+                expanded = false
+            } else {
+                view.findViewById<LinearLayout>(R.id.adv_list).visibility = View.VISIBLE
+                expanded = true
+            }
+        }
+        ArrayAdapter.createFromResource(
+            view.context,
+            R.array.sampler,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+        btnGenerate.setOnClickListener {
+            val prompt = etPrompt.text.toString()
+            val user = ParseUser.getCurrentUser()
+            submitPost(prompt, user)
+        }
+    }
+
+
+    private fun submitPost(prompt: String, user: ParseUser) {
+        post = posts()
+        post.setPrompt(prompt)
+        post.setUser(user)
+        val json = "{\"prompt\":\"$prompt\"}"
+        requestHeaders["apikey"] = GlobalVariableClass.api_key
+        requestHeaders["Accept"] = "application/json"
+        val client = AsyncHttpClient()
+        val body: RequestBody = json.toRequestBody(JSON)
+        client.post(GlobalVariableClass.api_generate_url, requestHeaders, params, body, object :
+            JsonHttpResponseHandler() {
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                response: String?,
+                throwable: Throwable?
+            ) {
+                Log.e(TAG, "onFailure $statusCode $response $headers")
+            }
+
+            override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON?) {
+                Log.e(TAG, "onSuccess $statusCode $json")
+                val jsonObject = json?.jsonObject
+                val id = jsonObject?.getString("id")
+                if (id != null) {
+                    val handler = Handler()
+                    handler.postDelayed({
+                        getImg(id)
+
+                    }, wait_time)
+                }
+            }
+        })
+
+        //TODO: Add a loading bar to indicate the saving of the post? or at least the generation of an image
+
+    }
+
+
+
+
+    //this function allows us to retrieve the image from the API call
     fun getImg(id: String) {
         val idUrl = GlobalVariableClass.api_retrieve_img_url + id
         val client = AsyncHttpClient()
@@ -93,83 +179,6 @@ class CreateTextFragment : Fragment() {
     }
 
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_text, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        etPrompt = view.findViewById<EditText>(R.id.etPrompt)
-        ivGenerated = view.findViewById<ImageView>(R.id.ivGenerated)
-        btnGenerate = view.findViewById<Button>(R.id.btnGenerate)
-        etPrompt = view.findViewById<EditText>(R.id.etPrompt)
-        spinner = view.findViewById(R.id.spinner)
-        view.findViewById<Button>(R.id.options_expand).setOnClickListener {
-            if (expanded) {
-                view.findViewById<LinearLayout>(R.id.adv_list).visibility = View.GONE
-                expanded = false
-            } else {
-                view.findViewById<LinearLayout>(R.id.adv_list).visibility = View.VISIBLE
-                expanded = true
-            }
-        }
-        ArrayAdapter.createFromResource(
-            view.context,
-            R.array.sampler,
-            android.R.layout.simple_spinner_item
-        ).also { adapter ->
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            spinner.adapter = adapter
-        }
-        btnGenerate.setOnClickListener {
-            val prompt = etPrompt.text.toString()
-            val user = ParseUser.getCurrentUser()
-            submitPost(prompt, user)
-        }
-    }
-
-    private fun submitPost(prompt: String, user: ParseUser) {
-        post = posts()
-        post.setPrompt(prompt)
-        post.setUser(user)
-        val json = "{\"prompt\":\"$prompt\"}"
-        requestHeaders["apikey"] = GlobalVariableClass.api_key
-        requestHeaders["Accept"] = "application/json"
-        val client = AsyncHttpClient()
-        val body: RequestBody = json.toRequestBody(JSON)
-        client.post(GlobalVariableClass.api_generate_url, requestHeaders, params, body, object :
-            JsonHttpResponseHandler() {
-            override fun onFailure(
-                statusCode: Int,
-                headers: Headers?,
-                response: String?,
-                throwable: Throwable?
-            ) {
-                Log.e(TAG, "onFailure $statusCode $response $headers")
-            }
-
-            override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON?) {
-                Log.e(TAG, "onSuccess $statusCode $json")
-                val jsonObject = json?.jsonObject
-                val id = jsonObject?.getString("id")
-                if (id != null) {
-                    val handler = Handler()
-                    handler.postDelayed({
-                        getImg(id)
-
-                    }, wait_time)
-                }
-            }
-        })
-
-        //TODO: Add a loading bar to indicate the saving of the post? or at least the generation of an image
-
-
-    }
 
 
     companion object {
