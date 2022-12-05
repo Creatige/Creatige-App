d750cd34dbe43f7070ee1834ad656266ddf93c97
Pedro Pacheco <pmpg@hotmail.com>
2022-12-04 20:40:57 -0500

    Profile fragment shows username and number of posts. Logout function disables the back button/gesture when opening the Login activity.

--- app/src/main/java/com/creatige/creatige/fragments/ProfileFragment.kt
+++ app/src/main/java/com/creatige/creatige/fragments/ProfileFragment.kt
@@ -2,14 +2,22 @@ package com.creatige.creatige.fragments
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.creatige.creatige.LoginActivity
 import com.creatige.creatige.R
+import com.creatige.creatige.posts
+import com.parse.ParseQuery
 import com.parse.ParseUser
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,6 +33,8 @@ class ProfileFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    lateinit var tvNumberPhotos: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,17 +56,55 @@ class ProfileFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         var btnLogout = view.findViewById<Button>(R.id.btnLogout)
+        var btnSettings = view.findViewById<ImageButton>(R.id.btnSettings)
+        var ivProfileImage = view.findViewById<ImageView>(R.id.ivProfileImage)
+        var tvUsername = view.findViewById<TextView>(R.id.tvUsername)
+        tvNumberPhotos = view.findViewById<TextView>(R.id.tvNumberPhotos)
+
+        var numberOfPosts = 0
+
 
         btnLogout.setOnClickListener {
+            logoutUser();
+        }
 
-            ParseUser.logOut()
-            ParseUser.getCurrentUser()
+        btnSettings.setOnClickListener {
+            // TODO: Uncomment when Settings activity is created
+            //var intent = Intent(context, SettingsActivity::class.java)
+            //startActivity(intent)
+        }
 
-            var intent = Intent(context, LoginActivity::class.java)
-            startActivity(intent)
+        Glide.with(this@ProfileFragment).load(ParseUser.getCurrentUser().getParseFile("avatar")?.url).into(ivProfileImage)
+        tvUsername.text = ParseUser.getCurrentUser().username
+        findNumberPosts()
 
+    }
+
+    private fun logoutUser() {
+        ParseUser.logOut()
+        ParseUser.getCurrentUser()
+
+        var intent = Intent(context, LoginActivity::class.java)
+        // This prevents the user to go back to the previous activity, which causes the app to crash as there is no user logged in
+        // It clears the activity stack
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
+    }
+
+    // function to get the number of photos the user has posted
+
+    private fun findNumberPosts(){
+        var query = ParseQuery.getQuery(posts::class.java)
+        query.whereEqualTo("user", ParseUser.getCurrentUser())
+        query.countInBackground { count, e ->
+            if (e == null) {
+                tvNumberPhotos.text =  "${count} images generated"
+            } else {
+                Log.e("ProfileFragment", "Error counting posts: $e")
+            }
         }
     }
+
 
     companion object {
         /**
